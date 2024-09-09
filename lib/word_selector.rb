7c6bb0b56d06197c3305fab340dc5db7a287a723
bjornrohrer <barohrerdk@gmail.com>
2024-09-09 14:36:23 +0200

    Wrapped my method in a module.

--- lib/word_selector.rb
+++ lib/word_selector.rb
@@ -1,10 +1,10 @@
-def random_word
-  words = File.readlines('google-10000-english-no-swears.txt').map(&:strip)
-  valid_words = words.select { |word| (5..12).include?(word.length) }
-  puts "The word is #{valid_words.sample.length} letters long"
+module WordSelector
+  def random_word
+    words = File.readlines('google-10000-english-no-swears.txt').map(&:strip)
+    valid_words = words.select { |word| (5..12).include?(word.length) }
+    puts "The word is #{valid_words.sample.length} letters long"
+  end
 end
 
 # Select a random word between 5 and 12 characters long from the google dictionary.
 #tell the user how long the word is.  
-
-puts random_word
