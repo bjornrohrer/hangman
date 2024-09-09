bd2dc44d9ac6007adbb298a6ef20225ccc7be31c
bjornrohrer <barohrerdk@gmail.com>
2024-09-09 14:56:24 +0200

    Created user guess.

--- lib/user_guess.rb
+++ lib/user_guess.rb
@@ -1,7 +1,11 @@
 module UserGuess
   def user_letter(letter)
-    
-    
+    letter = gets.chomp
+    if random_word.include?(letter)
+      puts "Correct #{letter} was included in the word."
+    else 
+      puts "Wrong #{letter} was not included in the word"
+    end
   end
 end
 
