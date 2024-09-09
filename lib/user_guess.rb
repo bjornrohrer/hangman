7bce41ca36a38c352b0f0b4c4e34c9a8f1597f60
bjornrohrer <barohrerdk@gmail.com>
2024-09-09 15:01:59 +0200

    Removed word selector file and added the code to a hangman class.

--- lib/user_guess.rb
+++ lib/user_guess.rb
@@ -1,5 +1,12 @@
-module UserGuess
+class Hangman
+  def random_word
+    words = File.readlines('google-10000-english-no-swears.txt').map(&:strip)
+    valid_words = words.select { |word| (5..12).include?(word.length) }
+    puts "The word is #{valid_words.sample.length} letters long"
+  end
+
   def user_letter(letter)
+    puts "Guess a letter"
     letter = gets.chomp
     if random_word.include?(letter)
       puts "Correct #{letter} was included in the word."
@@ -8,4 +15,8 @@ module UserGuess
     end
   end
 end
+
+
+
+
 
