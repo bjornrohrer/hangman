a9c27c2d674de2b6ef1307ae0d546f31c506172e
bjornrohrer <barohrerdk@gmail.com>
2024-09-09 15:08:13 +0200

    Change to instance variables.

--- lib/user_guess.rb
+++ lib/user_guess.rb
@@ -1,14 +1,16 @@
 class Hangman
   def random_word
     words = File.readlines('google-10000-english-no-swears.txt').map(&:strip)
-    valid_words = words.select { |word| (5..12).include?(word.length) }
+    @valid_words = words.select { |word| (5..12).include?(word.length) }
+    @random_word = @valid_words.sample
     puts "The word is #{valid_words.sample.length} letters long"
+    @random_word
   end
 
-  def user_letter(letter)
+  def user_letter
     puts "Guess a letter"
-    letter = gets.chomp
-    if random_word.include?(letter)
+    letter = gets.chomp.downcase 
+    if @random_word.include?(letter)
       puts "Correct #{letter} was included in the word."
     else 
       puts "Wrong #{letter} was not included in the word"
@@ -16,7 +18,6 @@ class Hangman
   end
 end
 
-
-
-
+new_game = Hangman.new
+puts new_game.random_word.user_letter()
 
