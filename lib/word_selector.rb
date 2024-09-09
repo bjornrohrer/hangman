1fb74f90026267bb5ee7926a4280fc7c306e99a0
bjornrohrer <barohrerdk@gmail.com>
2024-09-09 14:21:15 +0200

    Changed method name.

--- lib/word_selector.rb
+++ lib/word_selector.rb
@@ -1,4 +1,4 @@
-def select_random_word
+def random_word
   words = File.readlines('google-10000-english-no-swears.txt').map(&:strip)
   valid_words = words.select { |word| (5..12).include?(word.length) }
   puts "The word is #{valid_words.sample.length} letters long"
@@ -7,4 +7,4 @@ end
 # Select a random word between 5 and 12 characters long from the google dictionary.
 #tell the user how long the word is.  
 
-puts select_random_word
+puts random_word
