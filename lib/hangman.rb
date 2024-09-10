5cd7b790a17db9b4d9a29743a920952aa607154d
bjornrohrer <barohrerdk@gmail.com>
2024-09-10 16:30:30 +0200

    Added Array for wordprogress.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -11,6 +11,7 @@ class Hangman
     words = File.readlines('google-10000-english-no-swears.txt').map(&:strip)
     @valid_words = words.select { |word| (5..12).include?(word.length) }
     @random_word = @valid_words.sample
+    @word_progress = Array.new(@random_word.length, '_')
     puts "The word is #{@random_word.length} letters long"
     @random_word
   end
